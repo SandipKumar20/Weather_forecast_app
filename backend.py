9d86d1d7dbd506b5c0dcb28c992e29b4996c24b7
Sandip <samyorangy@gmail.com>
2024-03-19 05:01:39 +0530

    Add temperature and sky features in the app

--- backend.py
+++ backend.py
@@ -2,20 +2,19 @@ from api_key import APIkey
 import requests
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place="Mumbai", forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={APIkey}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     no_values = 8 * forecast_days
     filtered_data = filtered_data[:no_values]
-    match kind:
-        case "Temperature":
-            filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-        case "Sky":
-            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=2, kind="Temperature"))
+    print(get_data(place="Mumbai", forecast_days=2))
+    f_data = get_data(place="Mumbai", forecast_days=2)
+    print([dict["dt_txt"] for dict in f_data])
+    print([type(dict) for dict in f_data])
+
